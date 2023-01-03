7b2dfe2431b16057355359da5f45f5c21346cc8a
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-01-03 22:25:56 +0100

    feat: send information of the form to the api and stock the response token in the local storage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
 import {AuthService} from "../../service/auth.service";
+import {publish} from "rxjs";
 
 
 @Component({
@@ -20,20 +21,29 @@ export class LoginComponent implements OnInit {
   })
 
   connexion = new FormGroup({
-    login: new FormControl(''),
-    pass: new FormControl(''),
+    login: new FormControl<string>(''),
+    pass: new FormControl<string>(''),
   })
 
   ngOnInit(): void {
   }
 
   onFormSubmitAuth() {
-    this.authService.login(this.connexion.value as { login: string, pass: string }, () => {
-      console.log('connected')
+    this.authService.login({
+      email: this.connexion.controls['login'].value ?? '',
+      mdp: this.connexion.controls['pass'].value ?? ''
+    }, (response) => {
+      const token: string  = response.token;
+      localStorage.setItem("token", token);
     })
   }
 
   onFormSubmitIns() {
-
+    this.authService.register({
+      email: this.inscription.controls['login'].value ?? '',
+      mdp: this.inscription.controls['pass'].value ?? '',
+      name: this.inscription.controls['prenom'].value ?? ''
+    }, (response) => {
+    })
   }
 }
