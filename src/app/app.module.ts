33e903f345364c2bde970733aad20e63a87b7cd7
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-01-03 22:26:03 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ButtonComponent } from './navbar/button/button.component';
-import { FooterComponent } from './footer/footer.component';
-import { PresentationComponent } from './presentation/presentation.component';
-import { CompetenceComponent } from './competence/competence.component';
-import { ProjetComponent } from './projet/projet.component';
-import { ContactComponent } from './contact/contact.component';
-import { GridComponent } from './competence/grid/grid.component';
-import { CercleComponent } from './competence/grid/cercle/cercle.component';
-import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {ButtonComponent} from './navbar/button/button.component';
+import {FooterComponent} from './footer/footer.component';
+import {PresentationComponent} from './presentation/presentation.component';
+import {CompetenceComponent} from './competence/competence.component';
+import {ProjetComponent} from './projet/projet.component';
+import {ContactComponent} from './contact/contact.component';
+import {GridComponent} from './competence/grid/grid.component';
+import {CercleComponent} from './competence/grid/cercle/cercle.component';
+import {AppRoutingModule} from './app-routing.module';
+import {LoginComponent} from './login/login.component';
+import {HomeComponent} from './home/home.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -32,9 +34,12 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
